8a5227c39951a74e6e88975c618a76ecbe6677d1
MiryangJung <miryang.dev@gmail.com>
2019-06-22 00:49:50 +0900

    Feat : Update view count

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const List = require("../models/list");
 
 router.get('/', (req, res) => {
     res.send("Telegram @TodayDevblog_bot");
 });
 
 router.get('/article', (req,res)=>{
-    res.redirect(req.query.url);
+    url = req.query.url;
+
+    List.findOneAndUpdate({link:url}, {$set:{view:view+1}}, function(err) {
+        if(err) console.log('View Update Error : '+err);
+    });
+
+    res.redirect(url);
 });
 
 module.exports = router;
